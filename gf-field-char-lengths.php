27a9b976f70d9652a3e72fe89e36e311ffd28dd0
Clifford Paulick <tko@tourkick.com>
2018-10-26 14:58:53 -0500

    Allow 'field_id' as array, better sanitization
    - Add get_int_before_after_period()
    - Add example usage of array as 'field_id' arg

--- gf-field-char-lengths.php
+++ gf-field-char-lengths.php
@@ -42,12 +42,12 @@ class GW_Minimum_Characters {
 	public function set_defaults() {
 		$this->defaults = [
 			'form_id'                => 0,
-			'field_id'               => 0,
+			'field_id'               => '',
 			'min_chars'              => 0,
 			'max_chars'              => - 1, // negative one for unlimited
 			'validation_message'     => false,
-			'min_validation_message' => __( 'Please enter at least %d characters.' ),
-			'max_validation_message' => __( 'You may only enter %d characters.' ),
+			'min_validation_message' => esc_html__( 'Please enter at least %d characters.' ),
+			'max_validation_message' => esc_html__( 'You may only enter %d characters.' ),
 		];
 	}
 
@@ -59,30 +59,91 @@ class GW_Minimum_Characters {
 		$this->sanitize_args();
 
 		// only add hook if data makes sense
-		if ( true === $this->args_are_valid() ) {
-			add_filter( "gform_field_validation_{$this->args['form_id']}_{$this->args['field_id_int']}", [ $this, 'validate_character_count' ], 10, 4 );
+		if ( false === $this->args_are_valid() ) {
+			return;
+		}
+
+		foreach ( $this->args['field_id_ints'] as $field_id_int ) {
+			add_filter( "gform_field_validation_{$this->args['form_id']}_{$field_id_int}", [ $this, 'validate_character_count' ], 10, 4 );
 		}
 	}
 
 	/**
 	 * Sanitize the args, which runs after wp_parse_args().
 	 *
-	 * Also sets $this->args['field_id_int'], which is required since the Gravity Forms hook requires it.
+	 * Sets $this->args['field_ids']
 	 */
 	public function sanitize_args() {
 		// Form ID must be an integer
 		$this->args['form_id'] = absint( $this->args['form_id'] );
 
-		// Field ID must be numeric, such as 7.2 for address field
-		$this->args['field_id'] = (float) $this->args['field_id'];
+		// Field ID may be a string or array (multiple Field IDs getting the same min, max, and messaging)
+		$this->args['field_id'] = (array) $this->args['field_id'];
+		$this->args['field_id'] = array_filter( $this->args['field_id'] );
+		$this->args['field_id'] = array_unique( $this->args['field_id'] );
 
-		// Set the integer value of the Field ID, used by the Gravity Forms hook
-		$this->args['field_id_int'] = absint( floor( $this->args['field_id'], 0 ) );
+		// Set $this->args['field_ids'] and $this->args['field_id_ints']
+		$this->args['field_ids']     = [];
+		$this->args['field_id_ints'] = [];
+
+		foreach ( $this->args['field_id'] as $field_id ) {
+			// Field ID must be numeric, such as 7.2 for "Address Line 2" from Field #7.
+			$field_id = (float) $field_id;
+
+			$this->args['field_ids'][] = $field_id;
+
+			// Set the integer value of the Field ID, used by the Gravity Forms hook
+			$this->args['field_id_ints'][] = $this->get_int_before_after_period( $field_id );
+		}
+
+		$this->args['field_id_ints'] = array_filter( $this->args['field_id_ints'] );
+		$this->args['field_id_ints'] = array_unique( $this->args['field_id_ints'] );
 
 		// Min and Max must be integers
 		$this->args['min_chars'] = absint( $this->args['min_chars'] );
 		// But Max could be -1 for unlimited
 		$this->args['max_chars'] = (int) $this->args['max_chars'];
+	}
+
+	/**
+	 * Get the integer value of the "before the period" or the "after the period" of a number.
+	 *
+	 * @param int|float|string $number
+	 * @param bool             $before
+	 *
+	 * @return bool|int
+	 */
+	private function get_int_before_after_period( $number, $before = true ) {
+		if ( ! is_numeric( $number ) ) {
+			return false;
+		}
+
+		$periods_count = substr_count( $number, '.' );
+
+		// Invalid if more than 1 period
+		if ( 1 < $periods_count ) {
+			return false;
+		}
+
+		$number = (string) $number;
+
+		// if an integer, add ".0" to the end
+		if ( 0 === $periods_count ) {
+			$number = $number . '.0';
+		}
+
+		// if starts with a period, add leading zero
+		if ( 0 === strpos( $number, '.' ) ) {
+			$number = '0' . $number;
+		}
+
+		$array = explode( '.', $number );
+
+		if ( $before ) {
+			return (int) $array[0];
+		} else {
+			return (int) $array[1];
+		}
 	}
 
 	/**
@@ -94,8 +155,15 @@ class GW_Minimum_Characters {
 		// Major fails
 		if (
 			empty( $this->args['form_id'] )
-			|| 0 > $this->args['field_id']
 			|| - 1 > $this->args['max_chars']
+		) {
+			return false;
+		}
+
+		// Setting Field IDs via sanitization didn't go as expected
+		if (
+			empty( $this->args['field_id_ints'] )
+			|| empty( $this->args['field_ids'] )
 		) {
 			return false;
 		}
@@ -135,59 +203,68 @@ class GW_Minimum_Characters {
 	 * @return array
 	 */
 	public function validate_character_count( $result, $value, $form, $field ) {
-		// only check if valid if comes through as valid already
-		if ( empty( $result['is_valid'] ) ) {
-			return $result;
-		}
+		foreach ( $this->args['field_ids'] as $field_id ) {
+			if ( ! empty( $result['is_valid'] ) ) {
+				break;
+			}
 
-		$field_id = $this->args['field_id'];
+			$field_id_before_period = $this->get_int_before_after_period( $field_id );
 
-		if ( is_array( $value ) ) {
-			if ( ! empty( $value[$field_id] ) ) {
-				$our_val = $value[$field_id];
+			// if looking for fields 7.2, 7.3, and 8.2, and we're processing Field ID 8, then skip 7.2 and 7.3
+			if (
+				empty( $field_id_before_period )
+				|| $field_id_before_period !== absint( $field['id'] )
+			) {
+				continue;
+			}
+
+			if ( is_array( $value ) ) {
+				if ( ! empty( $value[$field_id] ) ) {
+					$our_val = $value[$field_id];
+				} else {
+					// Field ID not found within the array, so bail
+					return $value;
+				}
 			} else {
-				// Field ID not found within the array, so bail
+				$our_val = $value;
+			}
+
+			// bail if unexpected value, such as boolean, array, object, or resource
+			// similar to https://secure.php.net/manual/function.is-scalar.php but we don't want booleans
+			if (
+				! is_string( $our_val )
+				&& ! is_float( $our_val )
+				&& ! is_int( $our_val )
+			) {
 				return $value;
 			}
-		} else {
-			$our_val = $value;
-		}
 
-		// bail if unexpected value, such as boolean, array, object, or resource
-		// similar to https://secure.php.net/manual/function.is-scalar.php but we don't want booleans
-		if (
-			! is_string( $our_val )
-			&& ! is_float( $our_val )
-			&& ! is_int( $our_val )
-		) {
-			return $value;
-		}
+			// do our custom validation
+			$char_count = strlen( $our_val );
 
-		// do our custom validation
-		$char_count      = strlen( $our_val );
-
-		if ( $char_count >= $this->args['min_chars'] ) {
-			$is_min_reached = true;
-		} else {
-			$is_min_reached = false;
-		}
-
-		if ( -1 === $this->args['max_chars'] ) {
-			$is_max_exceeded = false;
-		} else {
-			if ( $char_count > $this->args['max_chars'] ) {
-				$is_max_exceeded = true;
+			if ( $char_count >= $this->args['min_chars'] ) {
+				$is_min_reached = true;
 			} else {
-				$is_max_exceeded = false;
+				$is_min_reached = false;
 			}
-		}
 
-		if ( ! empty( $is_min_reached ) ) {
-			$result['is_valid'] = false;
-			$result['message']  = sprintf( $this->args['min_validation_message'], $this->args['min_chars'] );
-		} else if ( $is_max_exceeded ) {
-			$result['is_valid'] = false;
-			$result['message']  = sprintf( $this->args['max_validation_message'], $this->args['max_chars'] );
+			if ( - 1 === $this->args['max_chars'] ) {
+				$is_max_exceeded = false;
+			} else {
+				if ( $char_count > $this->args['max_chars'] ) {
+					$is_max_exceeded = true;
+				} else {
+					$is_max_exceeded = false;
+				}
+			}
+
+			if ( ! empty( $is_min_reached ) ) {
+				$result['is_valid'] = false;
+				$result['message']  = sprintf( $this->args['min_validation_message'], $this->args['min_chars'] );
+			} else if ( $is_max_exceeded ) {
+				$result['is_valid'] = false;
+				$result['message']  = sprintf( $this->args['max_validation_message'], $this->args['max_chars'] );
+			}
 		}
 
 		return $result;
@@ -221,5 +298,18 @@ new GW_Minimum_Characters(
 		'max_chars'              => 30,
 		'min_validation_message' => esc_html__( 'Oops! Address Line 1 must be at least %d characters.' ),
 		'max_validation_message' => esc_html__( 'Oops! Address Line 1 must be %d or fewer characters.' )
+	]
+);
+
+/**
+ * Example Usage: Field 1 from Form 6 is a Name field and therefore Field ID 1.3 is the First Name and 1.6 is the
+ * Last Name and both have the same validation of 2-40 characters long. Use the default validation message text.
+ */
+new GW_Minimum_Characters(
+	[
+		'form_id'   => 6,
+		'field_id'  => [ 1.2, 1.3 ],
+		'min_chars' => 2,
+		'max_chars' => 40,
 	]
 );
